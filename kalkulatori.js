f6b6f8321af863bda36eece0ef1e95a805eb36d4
annikagashi123 <annikagashi18@gmail.com>
2024-11-21 09:46:22 -0800

    punovaa

---
+++ kalkulatori.js
@@ -0,0 +1,17 @@
+let numri1 =  +prompt("shkruaj numrin e pare");
+let operatorin =  prompt("shkruaj operatorin");
+let numri2 = +prompt("shkruaj numrin e dyte");
+
+
+if (operatorin == '+' ) {
+    alert(numri1 + numri2)
+}
+if (operatorin == '-' ) {
+    alert(numri1 - numri2)
+}
+if (operatorin == '*' ) {
+    alert(numri1 * numri2)
+}
+if (operatorin == '/' ) {
+    alert(numri1 / numri2)
+}
---
+++ script.js
@@ -0,0 +1,31 @@
+
+//let x = 4 + 4;
+//operatoret unar
+// let numri1 = -4;
+// //comcole.log(numri1);
+// // e shendrronvleren ne number
+// let numri2 = +"4";
+// let numri3 = +false;
+
+// //inkrement rritje
+
+// let numri = 0;
+
+// console.log("rreshti nr 14", ++numri);    //preinkriment e rrit numrin rrehtin e njejte
+// console.log("rreshti nr 15", numri ++);    //pasinkrement e rrit numrin por njr rresht me vone
+// console.log("rreshti nr 16", numri);    //nuk ndodh asgje por del dz per shkak te rreshtit te 15
+
+
+// //dekrement ==> zbritje
+// console.log("rreshti nr 20", --numri);
+// console.log("rreshti nr 21", numri--);
+// console.log("rreshti nr 22" , numri);
+
+
+// let bora = confirm("a po bjen bore");
+// if (bora  ) {
+//     alert("kron rri n shpi");
+// }
+// if (bora  ) {
+//     alert("mos shko ne shpi");
+// }
